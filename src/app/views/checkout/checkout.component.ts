8b2a9efc8b07ac9d1342b3815e688acfb5266dd0
duydq2 <duydq2@vnext.com.vn>
2020-11-21 10:22:13 +0700

    update code header, checkout, product

--- src/app/views/checkout/checkout.component.ts
+++ src/app/views/checkout/checkout.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
+  isShow = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectCheckBox(){
+    this.isShow = !this.isShow
+  }
 }
